404dbcd4dbd988bfb90a763b16331d410bfa67d3
halionaz <samhsj@naver.com>
2023-10-29 23:59:44 +0900

    🚧 Making Editable Editor

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,24 +1,41 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import style from "./Editor.module.css";
 import uid from "../Uid.js";
+import EditableBlock from "../Block/EditableBlock";
 
-const initialBlock = {id : uid(), html: "에디터일겁니다", tag: "p"};
+const initialBlock = { id: uid(), html: "에디터일겁니다", tag: "p" };
 
 const Editor = () => {
-
     const [blocks, setBlocks] = useState([initialBlock]);
 
-    return <div className={style.editor}>
-        {blocks.map((block, key) => {
-            return (
-                <div key={key} id={block.id}>
-                    Tag: {block.tag}, Content: {block.html}
-                </div>
-            )
-        })}
-    </div>
+    const updateEditorHandler = (updatedBlock) => {
+        const index = blocks.map((block) => block.id).indexOf(updatedBlock.id);
+        const updatedBlocks = [...blocks];
+        updatedBlocks[index] = {
+            ...updatedBlocks[index],
+            tag: updatedBlock.tag,
+            html: updatedBlock.html,
+        };
+        setBlocks(updatedBlocks);
+    };
+
+    return (
+        <div className={style.editor}>
+            {blocks.map((block, key) => {
+                return (
+                    <EditableBlock
+                        key={key}
+                        id={block.id}
+                        tag={block.tag}
+                        html={block.html}
+                        updateEditor={updateEditorHandler}
+                    />
+                );
+            })}
+        </div>
+    );
 };
 
 export default Editor;
