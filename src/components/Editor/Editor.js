13113107e73fb9d2d249d60db4f36940cba54885
halionaz <samhsj@naver.com>
2023-10-29 18:59:34 +0900

    💥 Introduce Block Edit System

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,7 +1,23 @@
+"use client"
+
+import { useState } from "react";
 import style from "./Editor.module.css";
 
+const initialBlock = {id : 0, html: "에디터일겁니다", tag: "p"};
+
 const Editor = () => {
-    return <div className={style.editor} contentEditable={true}>에디터일겁니다</div>;
+
+    const [blocks, setBlocks] = useState([initialBlock]);
+
+    return <div className={style.editor}>
+        {blocks.map((block, key) => {
+            return (
+                <div key={key} id={block.id}>
+                    Tag: {block.tag}, Content: {block.html}
+                </div>
+            )
+        })}
+    </div>
 };
 
 export default Editor;
