a668bf5701f6044a76f870273861c6585256fabd
halionaz <samhsj@naver.com>
2023-10-30 23:41:00 +0900

    🚚 Move & Rename resources

--- src/components/utility/uid.js
+++ src/components/utility/uid.js
@@ -1,7 +1,7 @@
 // 유니크한 ID 부여를 위한 함수
 
-const Uid = () => {
+const uid = () => {
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
 };
 
-export default Uid;
+export default uid;
