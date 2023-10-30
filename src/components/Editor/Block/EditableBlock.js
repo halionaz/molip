8dfbeb9fd0e406c1ac8056f3479f6b61aebed311
halionaz <samhsj@naver.com>
2023-10-31 00:22:31 +0900

    🚑️ Fix react-contenteditable's Bug

--- src/components/Editor/Block/EditableBlock.js
+++ src/components/Editor/Block/EditableBlock.js
@@ -1,21 +1,27 @@
-import { useState } from "react";
-import ContentEditable from "react-contenteditable";
+import { useRef, useState } from "react";
+import ContentEditable from "@/components/utility/content-editable";
 
-const EditableBlock = ({ id, tag, html}) => {
-
-    const [content, setContent] = useState({ id, tag, html });
+const EditableBlock = ({ id, tag, html }) => {
+    const ref = useRef(null);
+    const [content, setContent] = useState({ tag, html });
 
     const onChangeHandler = (e) => {
         // 입력 시 content에 담김
         setContent({ ...content, html: e.target.value });
-    }
+    };
+
+    const onBlurHandler = () => {
+        console.log(content);
+    };
 
     return (
         <ContentEditable
             className="block"
+            innerRef={ref}
             tagName={content.tag}
             html={content.html}
             onChange={onChangeHandler}
+            onBlur={onBlurHandler}
         />
     );
 };
