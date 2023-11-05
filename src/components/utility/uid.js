99929b80fd8259d6a13c6f12a2a3f0c2923df5e3
halionaz <samhsj@naver.com>
2023-11-05 22:05:10 +0900

    ⚡️ Change UID Method

--- src/components/utility/uid.js
+++ src/components/utility/uid.js
@@ -1,7 +1,7 @@
 // 유니크한 ID 부여를 위한 함수
 
 const uid = () => {
-    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+    return Date.now().toString(36) + Math.random().toString(36).substring(2);
 };
 
 export default uid;
