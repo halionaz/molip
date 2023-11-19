636a1865e7e6d8a0f1679d11282c512242094c0f
halionaz <samhsj@naver.com>
2023-11-19 13:01:20 +0900

    💄 Edit Right Sidebar Design

--- src/components/Sidebar/RightSidebar.js
+++ src/components/Sidebar/RightSidebar.js
@@ -1,5 +1,5 @@
 import styles from "./RightSidebar.module.css";
-import { GoQuestion, GoGear } from "react-icons/go";
+import { GoQuestion, GoGear, GoSearch, GoHistory, GoListUnordered } from "react-icons/go";
 
 const RightSidebar = () => {
     return (
@@ -10,7 +10,26 @@ const RightSidebar = () => {
                     <GoGear className={styles.headerIcon} />
                     <div className={styles.profile}></div>
                 </header>
-                <div className={styles.bars}></div>
+                <div className={styles.bars}>
+                    <div className={styles.mainBar}>
+                        <div
+                            className={[
+                                styles.mainBarComponent,
+                                styles.selected,
+                            ].join(" ")}
+                        >
+                            <GoListUnordered className={styles.mainBarBtn} />
+                        </div>
+                        <div className={styles.mainBarComponent}>
+                            <GoSearch className={styles.mainBarBtn} />
+                        </div>
+                        <div className={styles.mainBarComponent}>
+                            <GoHistory className={styles.mainBarBtn} />
+                        </div>
+                    </div>
+                    <div className={styles.subBar}>
+                    </div>
+                </div>
             </nav>
         </div>
     );
