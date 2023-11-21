4244cb560709530ed8822267ccc19741dd6fdc82
halionaz <samhsj@naver.com>
2023-11-21 16:56:10 +0900

    🗃️ Connect To Database Server

--- src/mongodb-server.js
+++ src/mongodb-server.js
@@ -1,20 +1,37 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const corsOption = {
-    origin : "http://localhost:3000"
-};
+const port = process.env.PORT || 8080;
+const conn_str = process.env.DB_CONNECTION_STRING;
 
-const PORT = process.env.PORT || 8080;
+// CONNECT TO MONGODB SERVER
+mongoose.connect(conn_str, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
 
-// Set CORS option
-app.use(cors(corsOption));
+// db 연결 여부 체크
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+    console.log("Connected to MongoDB Atlas");
+});
 
-app.get("/", (req, res) => {
-    res.json({message : `Server is running on port ${PORT}`});
-})
+// const corsOption = {
+//     origin : "http://localhost:3000"
+// };
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+// // Set CORS option
+// app.use(cors(corsOption));
+
+// app.get("/", (req, res) => {
+//     res.json({message : `Server is running on port ${port}`});
+// })
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
